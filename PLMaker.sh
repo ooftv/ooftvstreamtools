52e6d69462264420a676d88cbee34d84afb3ee70
Loren Risker <outoffocustv@gmail.com>
2019-01-27 19:08:47 -0800

    updated PLmaker

--- PLMaker.sh
+++ PLMaker.sh
@@ -1,25 +1,24 @@
-# this scripts 
+# this scripts
 # 1) Finds Videos
 # 2) Puts them into a playlist
 # 3) Shuffles them up
-# 4) Makes sure certian videos play every so many videos
-# for example it will play something from the promo folder every 10 videos
+# 4) (still in progress) Makes sure certian videos play every so many videos # for example it will play something from the promo folder every 10 videos
+
 
 # Paths to compressed videos:
 # Music Videos
-MPC='/Users/lorenrisker/Movies/ooftv/broadcast/sourcevideos/musicvideoplaylist-PLF06E26B33B6A2F33/compressed'
+musicvideos='../../broadcast/musicvideos'
 # Live Videos
-LPC='/Users/lorenrisker/Movies/ooftv/broadcast/sourcevideos/oofliveplaylist-PLA026E00FEC044A76/compressed'
-# Add To Stream Playlist
-SPC='/Users/lorenrisker/Movies/ooftv/broadcast/sourcevideos/oofaddtostreamplaylist-PLmvDDOT4vxZd4th9yXtHKXc6HSA_93nlV/compressed'
-# OOF Originals
-OPC='/Users/lorenrisker/Movies/ooftv/broadcast/sourcevideos/ooforiginals'
-# Promos
-# Shorts and extras
-# Old broadcast folder (should clean up, sort, and remove)
-OLD='/Users/lorenrisker/Movies/OOFTV/Broadcast/Approved for Broadcast/Compressed'
+livevideos='../../broadcast/live'
+# Other videos
+othervideos='../../broadcast/othervideos'
+# OOF Promos
+promos='../../broadcast/promos'
+# OOF Series
+series='../../broadcast/series'
+
 # Playlist Path
-listPath='/Users/lorenrisker/Movies/ooftv/broadcast/playlists'
+listPath='../../broadcast/playlists'
 
 # Playlist Creator
 d=$(date +%Y-%m-%d) # use this to give playlists a unique name
@@ -31,14 +30,15 @@ echo $shufList
 # Compile everything into a playlist
 # the -d prints the entire path instead of just the contents which is needed for this to work as a playlist
 # ls -d "$MPC"/*.mp4 >> $listPath/$listName # don't include music video playlist as its not vetted for rights
-ls -d "$LPC"/*.mp4 >> $listPath/$listName
-ls -d "$SPC"/*.mp4 >> $listPath/$listName
-ls -d "$OPC"/*.mp4 >> $listPath/$listname
-ls -d "$OLD"/*.mp4 >> $listPath/$listname
+ls -d "$musicvideos"/*.mp4 >> $listPath/$listName
+ls -d "$livevideos"/*.mp4 >> $listPath/$listName
+ls -d "$othervideos"/*.mp4 >> $listPath/$listname
+ls -d "$promos"/*.mp4 >> $listPath/$listname
+ls -d "$series"/*.mp4 >> $listPath/$listname
 
 # Shuffle the playlist
-# this relies on gshuf which is not installed natively. can be installed with brew install coreutils
+# this relies on gshuf which is not installed natively on a mac. can be installed with brew install coreutils
+# osmc may use shuf instead of gshuf
 # this uses tee to send the output to two files, one named for the date (for records) and one simply overwrites the playlist called today.m3u
 # obs (or whatever streamer you want) will just always point to the file called today.m3u which will be refreshed everytime the script is run
 /usr/local/bin/gshuf $listPath/$listName | tee $listPath/$shufList $listPath/today.m3u > /dev/null
-
